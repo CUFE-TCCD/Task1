3ccdb4f53070b20271c580a3a16039cdfe176a85
AnAs101AlAa <anas.ibrahim03@eng-st.cu.edu.eg>
2024-09-14 19:35:13 +0300

    added edit and add event ui

--- frontend/src/components/EventPosters/eventPoster.jsx
+++ frontend/src/components/EventPosters/eventPoster.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import PropTypes from 'prop-types';
 
-export default function EventPoster({ title, date, shortDescription, image }) {
+export default function EventPoster({ eventData, triggerEdit }) {
     const [fadeIn, setFadeIn] = useState('opacity-0 -translate-x-1/2');
-    const formattedDate = new Date(date).toLocaleDateString('en-US', {
+    const formattedDate = new Date(eventData.date).toLocaleDateString('en-US', {
         year: 'numeric',
         month: 'long',
         day: 'numeric'
@@ -22,27 +22,29 @@ export default function EventPoster({ title, date, shortDescription, image }) {
     return (
         <>
             <div className="group flex-none sm:flex hidden w-full h-full z-10 gap-4 border-b-2 border-[#a79d9d] py-4">
-                <img src={image} alt="event" className="w-1/3 object-fit rounded-lg -z-10" />
+                <img src={eventData.image} alt="event" className="w-1/3 object-fit rounded-lg -z-10" />
                 <div className={`w-full py-3 px-6 relative border-l-2 border-[#a79d9d] ${fadeIn} transition-all duration-[400ms] ease-in-out`}>
-                    <p className="font-bold text-2xl">{title}</p>
+                    <p className="font-bold text-2xl">{eventData.title}</p>
                     <p className="text-lg mt-1 font-medium">Date: {formattedDate}</p>
-                    <p className="text-md mt-2 font-medium leading-6">{shortDescription}</p>
-                    <div className="gap-6 flex mt-4">
-                        <button className="font-semibold px-4 py-1 rounded-lg bg-[#285D7C] hover:bg-white text-white hover:text-black hover:border-black border-transparent border transition-colors duration-200 ease-in-out">Learn more</button>
-                        <button className="font-semibold px-4 py-1 rounded-lg bg-[#cc3838] hover:bg-white text-white hover:text-black hover:border-black border-transparent border transition-colors duration-200 ease-in-out">Register</button>
+                    <p className="text-md mt-2 font-medium leading-6">Description: {eventData.shortDescription}</p>
+                    <div className="gap-5 flex mt-4">
+                        <button className="font-semibold px-4 py-1 rounded-lg bg-[#3d3d3d] hover:bg-white text-white hover:text-black hover:border-black border-transparent border transition-colors duration-200 ease-in-out">More Details</button>
+                        <button onClick={() => triggerEdit(eventData)} className="font-semibold px-4 py-1 rounded-lg bg-[#285D7C] hover:bg-white text-white hover:text-black hover:border-black border-transparent border transition-colors duration-200 ease-in-out">Edit</button>
+                        <button className="font-semibold px-4 py-1 rounded-lg bg-[#cc3838] hover:bg-white text-white hover:text-black hover:border-black border-transparent border transition-colors duration-200 ease-in-out">Delete</button>
                     </div>
                 </div>
             </div>
             <div className="relative group sm:hidden w-full min-h-[180px] z-10">
-                <img className="absolute top-0 h-full w-full object-fit z-0 rounded-lg" src={image} alt="event" />
+                <img className="absolute top-0 h-full w-full object-fit z-0 rounded-lg" src={eventData.image} alt="event" />
                 <div className="absolute w-full bottom-0 bg-black bg-opacity-55 h-1/3 group-hover:h-full z-10 transition-all duration-300 ease-in-out rounded-b-lg group-hover:rounded-lg" />
                 <div className="w-full h-full p-3 z-20 absolute top-0 translate-y-2/3 group-hover:translate-y-0 text-white transition-all duration-300 ease-in-out">
-                    <p className="text-sm">{title}</p>
+                    <p className="text-sm">{eventData.title}</p>
                     <p className="text-xs">{formattedDate}</p>
-                    <p className="text-xs mt-2 group-hover:block hidden">{shortDescription}</p>
+                    <p className="text-xs mt-2 group-hover:block hidden">Description: {eventData.shortDescription}</p>
                     <div className="gap-2 mt-2 group-hover:flex hidden">
-                        <button className="font-semibold px-2 py-1 rounded-lg text-xs bg-[#285D7C] hover:bg-white text-white hover:text-black hover:border-black border-transparent border transition-colors duration-200 ease-in-out">Learn more</button>
-                        <button className="font-semibold px-2 py-1 rounded-lg text-xs bg-[#cc3838] hover:bg-white text-white hover:text-black hover:border-black border-transparent border transition-colors duration-200 ease-in-out">Register</button>
+                        <button className="font-semibold px-4 py-1 rounded-lg bg-[#3d3d3d] hover:bg-white text-white hover:text-black hover:border-black border-transparent border transition-colors duration-200 ease-in-out">More Details</button>
+                        <button onClick={() => triggerEdit(eventData)} className="font-semibold px-2 py-1 rounded-lg text-xs bg-[#285D7C] hover:bg-white text-white hover:text-black hover:border-black border-transparent border transition-colors duration-200 ease-in-out">Edit</button>
+                        <button className="font-semibold px-2 py-1 rounded-lg text-xs bg-[#cc3838] hover:bg-white text-white hover:text-black hover:border-black border-transparent border transition-colors duration-200 ease-in-out">Delete</button>
                     </div>
                 </div>
             </div>
@@ -51,8 +53,6 @@ export default function EventPoster({ title, date, shortDescription, image }) {
 }
 
 EventPoster.propTypes = {
-    title: PropTypes.string.isRequired,
-    date: PropTypes.string.isRequired,
-    shortDescription: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired
+    eventData: PropTypes.object.isRequired,
+    triggerEdit: PropTypes.func.isRequired
 }
--- frontend/src/pages/admin/events_page/Event.jsx
+++ frontend/src/pages/admin/events_page/Event.jsx
@@ -2,8 +2,15 @@ import withSidebar from "@/components/HOC/withSidebar";
 import EventPoster from "@/components/EventPosters/eventPoster";
 import EventTypePoster from "@/components/EventPosters/eventTypePoster";
 import '/src/styles/eventPage.css';
+import NewEventForm from "./newEventForm";
+
+import { useState } from "react";
+import EditEventForm from "./editEventForm";
 
 const Event = () => {
+  const [addNewEvent, setAddNewEvent] = useState(false);
+  const [editEvent, setEditEvent] = useState(null);
+
   const events = [
     {
       title: 'Zayed Orientation Day ',
@@ -69,26 +76,31 @@ const Event = () => {
   ];
 
   return (
-    <>
-      <p className="text-[17px]">the collection of our new and excited events and our past milestones and successes</p>
-      <div className="flex flex-col w-full mt-6">
-        <p className='text-[30px] font-bold w-fit py-1'>upcoming events</p>
-        <div className="flex flex-col sm:gap-0 px-3 border-l-2 border-[#a79d9d] gap-4 h-full w-full select-none">
-          {events.map((event, index) => (
-            <EventPoster key={index} {...event} />
-          ))}
+    <div className={`w-full h-fit pb-6 relative ${addNewEvent}`}>
+      {addNewEvent && <NewEventForm setOpenForm={setAddNewEvent} />}
+      {editEvent !== null && <EditEventForm eventData={editEvent} setOpenForm={setEditEvent} />}
+      <div className={`w-full h-full ${addNewEvent || editEvent !== null ? "h-screen overflow-hidden" : ""}`}>
+        <p className="text-[17px] mt-3 md:mt-0">the collection of our new and excited events and our past milestones and successes</p>
+        <button onClick={() => setAddNewEvent(true)} className="font-semibold px-6 py-1 mt-2 rounded-lg bg-[#cc3838] hover:bg-white text-white hover:text-black hover:border-black border-transparent border transition-colors duration-200 ease-in-out">Add New Event</button>
+        <div className="flex flex-col w-full">
+          <p className='text-[30px] font-bold w-fit py-1'>upcoming events</p>
+          <div className="flex flex-col sm:gap-0 px-3 border-l-2 border-[#a79d9d] gap-4 h-full w-full select-none">
+            {events.map((event, index) => (
+              <EventPoster key={index} triggerEdit={setEditEvent} eventData={event}/>
+            ))}
+          </div>
         </div>
-      </div>
 
-      <div className="flex flex-col w-full mt-6">
-        <p className='text-[30px] font-bold w-fit py-1 mb-2'>Past events</p>
-        <div className="flex gap-2 py-2 overflow-x-auto custom-scrollbar h-full w-full">
-          {eventTypes.map((event, index) => (
-            <EventTypePoster key={index} {...event} />
-          ))}
+        <div className="flex flex-col w-full mt-6">
+          <p className='text-[30px] font-bold w-fit py-1 mb-2'>Past events</p>
+          <div className="flex gap-2 py-2 overflow-x-auto custom-scrollbar h-full w-full">
+            {eventTypes.map((event, index) => (
+              <EventTypePoster key={index} {...event} />
+            ))}
+          </div>
         </div>
       </div>
-    </>
+    </div >
   )
 };
 
