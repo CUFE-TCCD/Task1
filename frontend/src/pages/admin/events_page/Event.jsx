11ee88756148472b301767dc42296a36751c77aa
AnAs101AlAa <anas.ibrahim03@eng-st.cu.edu.eg>
2024-09-18 19:32:27 +0300

    changes

--- frontend/src/pages/admin/events_page/Event.jsx
+++ frontend/src/pages/admin/events_page/Event.jsx
@@ -1,60 +1,30 @@
 import withSidebar from "@/components/HOC/withSidebar";
-import EventPoster from "@/components/EventPosters/eventPoster";
-import EventTypePoster from "@/components/EventPosters/eventTypePoster";
+import EventPoster from "./pageComponents/EventPosters/eventPoster";
+import EventTypePoster from "./pageComponents/EventPosters/eventTypePoster";
+import FullEventView from "./fullEventView";
 import '/src/styles/eventPage.css';
 import NewEventForm from "./newEventForm";
-
-import { useState } from "react";
 import EditEventForm from "./editEventForm";
+
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Event = () => {
   const [addNewEvent, setAddNewEvent] = useState(false);
   const [editEvent, setEditEvent] = useState(null);
+  const [viewedEvent, setViewedEvent] = useState(null);
+  const [events, setEvents] = useState([]);
 
-  const events = [
-    {
-      title: 'Zayed Orientation Day ',
-      date: '2024-09-16',
-      shortDescription: 'we are ecxited to welcome our freshmen students and introduce them to the great entity they have become a part of and to TCCD with our activities and way of work, join us for this day to know more about us and our programs',
-      image: "/src/assets/tccdbackground.jpeg"
-    },
-    {
-      title: 'Event 2',
-      date: '2022-02-01',
-      shortDescription: 'This is a short description for event 2',
-      image: "/src/assets/tccdbackground.jpeg"
-    },
-    {
-      title: 'Event 3',
-      date: '2022-03-01',
-      shortDescription: 'This is a short description for event 3',
-      image: "/src/assets/tccdbackground.jpeg"
-    },
-    {
-      title: 'Event 4',
-      date: '2022-04-01',
-      shortDescription: 'This is a short description for event 4',
-      image: "/src/assets/tccdbackground.jpeg"
-    },
-    {
-      title: 'Event 5',
-      date: '2022-05-01',
-      shortDescription: 'This is a short description for event 5',
-      image: "/src/assets/tccdbackground.jpeg"
-    },
-    {
-      title: 'Event 6',
-      date: '2022-06-01',
-      shortDescription: 'This is a short description for event 6',
-      image: "/src/assets/tccdbackground.jpeg"
-    },
-    {
-      title: 'Event 7',
-      date: '2022-10-16',
-      shortDescription: 'This is a short description for event 7',
-      image: "/src/assets/tccdbackground.jpeg"
-    }
-  ];
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'auto' });
+
+    const fetchEvents = async () => {
+      const response = await axios.get('https://virtserver.swaggerhub.com/FAROUQDIAAELDIN/Task1TCCD/1.0.0/events');
+      setEvents(response.data);
+      console.log(response.data);
+    };
+    fetchEvents();
+  }, []);
 
   const eventTypes = [
     {
@@ -77,16 +47,17 @@ const Event = () => {
 
   return (
     <div className={`w-full h-fit pb-6 relative`}>
-      {addNewEvent && <NewEventForm setOpenForm={setAddNewEvent} openForm={addNewEvent}/>}
+      {addNewEvent && <NewEventForm setOpenForm={setAddNewEvent} openForm={addNewEvent} />}
       {editEvent !== null && <EditEventForm eventData={editEvent} setOpenForm={setEditEvent} />}
-      <div className={`w-full h-full ${addNewEvent || editEvent !== null ? "h-screen overflow-hidden" : ""}`}>
+      {viewedEvent !== null && <FullEventView setOpen={setViewedEvent} eventData={viewedEvent} />}
+      <div className={`w-full h-full ${addNewEvent || editEvent !== null || viewedEvent !== null ? "h-screen overflow-hidden" : ""}`}>
         <p className="text-[17px] mt-3 md:mt-0">the collection of our new and excited events and our past milestones and successes</p>
         <button onClick={() => setAddNewEvent(true)} className="font-semibold px-6 py-1 mt-2 rounded-lg bg-[#cc3838] hover:bg-white text-white hover:text-black hover:border-black border-transparent border transition-colors duration-200 ease-in-out">Add New Event</button>
         <div className="flex flex-col w-full">
           <p className='text-[30px] font-bold w-fit py-1'>upcoming events</p>
           <div className="flex flex-col sm:gap-0 px-3 border-l-2 border-[#a79d9d] gap-4 h-full w-full select-none">
-            {events.map((event, index) => ( 
-              <EventPoster key={index} triggerEdit={setEditEvent} eventData={event}/>
+            {events.map((event, index) => (
+              <EventPoster key={index} triggerEdit={setEditEvent} eventData={event} triggerDetails={setViewedEvent} />
             ))}
           </div>
         </div>
---
+++ frontend/src/pages/admin/events_page/fullEventView.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { FaRegImages } from "react-icons/fa";
+import { IoMdClose } from "react-icons/io";
+import PropTypes from 'prop-types';
+
+export default function FullEventView({ setOpen, eventData }) {
+
+    const formattedDate = new Date(eventData.date).toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    });
+
+    console.log(eventData);
+    return (
+        <div className={`absolute bg-white w-full h-svh pb-6 z-40 transition-all duration-[800ms] ease-in-out pr-5`}>
+            <div className="flex justify-between w-full">
+                <p className="text-[26px] font-bold">{eventData.title}</p>
+                <IoMdClose onClick={() => setOpen(null)} size={30} className="hover:cursor-pointer" />
+            </div>
+
+
+            <div className="flex-col gap-5 flex">
+                <div className="flex gap-2 py-2 overflow-x-auto custom-scrollbar h-1/3 max-h-[250px] w-full mt-2">
+                    {eventData.images.map((image, index) => (
+                        <img className="max-w-[444px]" src={image} key={index} />
+                    ))}
+                </div>
+                <p className="font-semibold text-xl flex gap-2">Date: <p className="font-normal text-lg">{formattedDate}</p></p>
+                <p className="font-semibold text-xl flex gap-2">Description: <p className="font-normal text-lg">{eventData.shortDescription}</p></p>
+                <div className="w-full">
+                    <p className="font-semibold text-xl">Registrations:</p>
+                        <table className="w-full h-fit border-2 border-black mt-2">
+                            <thead>
+                                <tr>
+                                    <th className="border-2 border-black">Name</th>
+                                    <th className="border-2 border-black">Email</th>
+                                    <th className="border-2 border-black">Phone</th>
+                                    <th className="border-2 border-black">Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td className="border border-black">John Doe</td>
+                                    <td className="border border-black">djwaoidj</td>
+                                    <td className="border border-black">123456789</td>
+                                    <td className="border border-black">2024-09-16</td>
+                                </tr>
+                            </tbody>
+                        </table>             
+                </div>
+            </div>
+
+
+        </div>
+    )
+
+}
+
+FullEventView.propTypes = {
+    setOpen: PropTypes.func.isRequired,
+    eventData: PropTypes.object.isRequired
+}
