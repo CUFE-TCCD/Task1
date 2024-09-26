1e4030e8ac563a467cc2ec7aec004e597a71589e
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2024-09-26 19:17:46 +0300

    fixed a bug

--- frontend/src/endpoints/authEndpoints.js
+++ frontend/src/endpoints/authEndpoints.js
@@ -1,11 +1,11 @@
 export const login = async (email, password) => {
   try {
-    const response = await fetch("http://localhost:5300/auth/login", {
+    const response = await fetch("http://localhost:5300/api/v1/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.parse({ email, password }),
+      body: JSON.stringify({ email, password }),
     });
     return response;
   } catch (error) {
@@ -15,12 +15,12 @@ export const login = async (email, password) => {
 
 export const signUp = async (sign_up_form) => {
   try {
-    const response = await fetch("http://localhost:5300/auth/signup", {
+    const response = await fetch("http://localhost:5300/api/v1/auth/signup", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.parse(sign_up_form),
+      body: JSON.stringify(sign_up_form),
     });
     return response;
   } catch (error) {
