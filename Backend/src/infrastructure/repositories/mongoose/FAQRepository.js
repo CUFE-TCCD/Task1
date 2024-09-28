8aa5f1bc09f03944dcde383722f9f9bce5c21dca
MostafaRabie <mostafamohammed27.mm@gmail.com>
2024-09-28 20:24:59 +0300

    add FAQ Repository

---
+++ Backend/src/infrastructure/repositories/mongoose/FAQRepository.js
@@ -0,0 +1,16 @@
+const FAQModel = require("../../database/mongoose/models/FAQSchema");
+
+class FAQRepository {
+  async createQuestion(question) {
+    return await FAQModel.create(question);
+  }
+
+  async updateAnswer(id, answer) {
+    return await FAQModel.findByIdAndUpdate(id, { answer }, { new: true });
+  }
+  async getAll() {
+    return await FAQModel.find().select("-__v");
+  }
+}
+
+module.exports = FAQRepository;
