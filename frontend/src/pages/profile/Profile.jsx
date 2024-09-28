4ec62b29f7f18b7e9b4fd837998c48a770babb46
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2024-09-28 04:49:30 +0300

    layout for task2 done, FAQs section done

---
+++ frontend/src/pages/profile/Profile.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import withNavbar from '../../components/HOC/withNavbar'
+
+const Profile = () => {
+  return (
+    <div>Profile</div>
+  )
+}
+
+export default withNavbar(Profile)
