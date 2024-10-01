c3740eefeac691576a0164baf9fa46fdf5cbe900
omarrrefaatt <119802537+omarrrefaatt@users.noreply.github.com>
2024-10-01 03:31:09 +0300

    default image

--- frontend/src/pages/sponsors/Sponsors.jsx
+++ frontend/src/pages/sponsors/Sponsors.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
 import withNavbar from "../../components/HOC/withNavbar";
-import { fetchSponsors } from "../../endpoints/sponsorsEndpoints"; // Mocked endpoint
+import { fetchSponsors } from "../../endpoints/sponsorsEndpoints";
 import { FaHouseFloodWaterCircleArrowRight } from "react-icons/fa6";
 
 const Sponsors = () => {
-  // State to hold sponsors data
   const [sponsorsData, setSponsorsData] = useState([
     {
       name: "Tech Corp",
@@ -13,47 +12,38 @@ const Sponsors = () => {
     },
     {
       name: "Innovate Inc.",
-      logo: "https://via.placeholder.com/150",
       info: "Driving innovation in the education sector.",
     },
     {
       name: "Green Energy",
-      logo: "https://via.placeholder.com/150",
       info: "Pioneering sustainable energy solutions worldwide.",
     },
     {
       name: "Tech Corp",
-      logo: "https://via.placeholder.com/150",
       info: "Leading tech solutions provider for over 20 years.",
     },
     {
       name: "Innovate Inc.",
-      logo: "https://via.placeholder.com/150",
       info: "Driving innovation in the education sector.",
     },
     {
       name: "Green Energy",
-      logo: "https://via.placeholder.com/150",
       info: "Pioneering sustainable energy solutions worldwide.",
     },
     {
       name: "Green Energy",
-      logo: "https://via.placeholder.com/150",
       info: "Pioneering sustainable energy solutions worldwide.",
     },
     {
       name: "Tech Corp",
-      logo: "https://via.placeholder.com/150",
       info: "Leading tech solutions provider for over 20 years.",
     },
     {
       name: "Innovate Inc.",
-      logo: "https://via.placeholder.com/150",
       info: "Driving innovation in the education sector.",
     },
     {
       name: "Green Energy",
-      logo: "https://via.placeholder.com/150",
       info: "Pioneering sustainable energy solutions worldwide.",
     },
   ]);
@@ -91,8 +81,16 @@ const Sponsors = () => {
               className="bg-white shadow-lg rounded-lg p-6 transition-transform duration-300 transform hover:scale-105"
             >
               <div className="w-32 h-32 bg-gray-300 mx-auto rounded-full flex items-center justify-center">
-                <span className="text-sm text-gray-500">{sponsor.image}</span>
+                <img
+                  src={
+                    sponsor.logo ||
+                    "https://scontent.fcai22-1.fna.fbcdn.net/v/t39.30808-6/457622186_1025691312682889_5724729561336696528_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=EL87MxY1-LIQ7kNvgFQ77CQ&_nc_ht=scontent.fcai22-1.fna&_nc_gid=AuumfDocibql9otSEhhSR0i&oh=00_AYD_1hlnvdhINNKeLuvRVKEnqSUKoWwkSUOZlz8xmrS75Q&oe=67011C92"
+                  }
+                  alt={sponsor.name}
+                  className="w-full h-full object-cover rounded-full"
+                />
               </div>
+
               <p className="mt-4">{sponsor.name}</p>
               <p className="text-sm mt-2">{sponsor.info}</p>
             </div>
